63eb6407750d3a83f5e92e9356a77c9557ab3c11
suzumiya_haruhi <suzumiya@haruhi.com>
2025-03-23 09:41:34 +0800

    dl .gitignore

---
+++ tutorial01/build/CMakeFiles/leptjson.dir/leptjson.c.o.d
@@ -0,0 +1,17 @@
+CMakeFiles/leptjson.dir/leptjson.c.o: \
+ /home/suzumiya/json_translation/json-tutorial/tutorial01/leptjson.c \
+ /usr/include/stdc-predef.h \
+ /home/suzumiya/json_translation/json-tutorial/tutorial01/leptjson.h \
+ /usr/include/assert.h /usr/include/features.h \
+ /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h
