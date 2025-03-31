8a873653e85d3b53256a05dbafa823c275a8d9ae
suzumiya_haruhi <suzumiya@haruhi.com>
2025-03-31 10:53:02 +0800

    增加注释

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -16,6 +16,10 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
+// 若一个 JSON 只含有空白，传回 LEPT_PARSE_EXPECT_VALUE。
+// 若一个值之后，在空白之后还有其他字符，传回 LEPT_PARSE_ROOT_NOT_SINGULAR。
+// 若值不是那三种字面值，传回 LEPT_PARSE_INVALID_VALUE。
+
 int lept_parse(lept_value* v, const char* json);
 
 lept_type lept_get_type(const lept_value* v);
